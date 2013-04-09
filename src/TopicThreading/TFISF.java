3044dd0380bdf5762d3c5e3ba8a31c77d2b8fcd9
Wenlong Tu <twlabc123@gmail.com>
2013-04-09 17:16:23 +0800

    add tfisf implement
    Use tfisf for clustering, it seems to be more reasonable than tfidf.

--- src/TopicThreading/TFISF.java
+++ src/TopicThreading/TFISF.java
@@ -3,6 +3,7 @@ package TopicThreading;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -16,13 +17,15 @@ import Structure.ArticleExtend;
 import Structure.Event;
 import Structure.Subtopic;
 
-public class TFIDF {
+public class TFISF {
 
 	public Vector<Subtopic> subtopic;
-	public HashMap<String, Integer> df;
-	public int docTotalNum;
+	public HashMap<String, Integer> sf;
+	//public int stTotalNum;
 	public double InitClusterThreshold = 0.1;
+	public double ThreadingThreshold = InitClusterThreshold;
 	public StopWordFilter swf;
+	public int TestSample = 7000; // just for test
 	
 	
 	/**
@@ -30,15 +33,15 @@ public class TFIDF {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TFIDF t = new TFIDF();
-		t.test("data/final/news_lc.txt", "data/final/news_lc_test.txt");
+		TFISF t = new TFISF();
+		t.test("data/final/news_lc.txt", "data/final/news_lc_test_tdisf.txt");
 	}
 	
-	TFIDF()
+	TFISF()
 	{
 		subtopic = new Vector<Subtopic>();
-		df = new HashMap<String, Integer>();
-		docTotalNum = 0;
+		sf = new HashMap<String, Integer>();
+		//stTotalNum = 0;
 		swf = new StopWordFilter();
 		swf.load("data/sogou/tf.csv");
 	}
@@ -66,6 +69,9 @@ public class TFIDF {
 			System.out.println("Threading");
 			Threading(reader, writer);
 			System.out.println("Threading finished");
+			System.out.println(sf.get("钓鱼岛/ns")+"/"+subtopic.size());
+			//System.out.println(sf.get("游行/n")+"/"+subtopic.size());
+			//System.out.println(sf.get("游行/v")+"/"+subtopic.size());
 			for (int j = 0; j<subtopic.size(); j++)
 			{
 				writer.println(subtopic.elementAt(j).summary+"\n<subtopic>");
@@ -81,7 +87,7 @@ public class TFIDF {
 		}
 	}
 	
-	public void init(Vector<Event> event, PrintWriter writer)
+	public void init(Vector<Event> event, PrintWriter writer) throws IOException
 	{
 		for (int i = 0; i<event.size(); i++)
 		{
@@ -89,18 +95,20 @@ public class TFIDF {
 			Event e = event.elementAt(i);
 			st.start = e.start;
 			st.end = e.end;
+			//stTotalNum++;
+			HashSet<String> temp = new HashSet<String>();
 			for (int j = 0; j<e.article.size(); j++)
 			{
-				docTotalNum++;
 				ArticleExtend a = e.article.elementAt(j);
 				st.docNum++;
 				if (st.summary.length() != 0) st.summary += "\n"+a.title; else st.summary += a.title;
 				String[] ss = a.content.split(" ");
-				HashSet<String> temp = new HashSet<String>();
+				HashSet<String> temp2 = new HashSet<String>();
 				for (int k = 0; k<ss.length; k++)
 				{
 					String term = ss[k];
 					if (swf.isStopWord(term)) continue;
+					
 					if (st.tf.containsKey(term))
 					{
 						Integer tempI = st.tf.get(term);
@@ -111,7 +119,23 @@ public class TFIDF {
 					{
 						st.tf.put(term, 1);
 					}
+					
 					if (!temp.contains(term))
+					{	
+						if (sf.containsKey(term))
+						{
+							Integer tempI = sf.get(term);
+							sf.remove(term);
+							sf.put(term, tempI+1);
+						}
+						else
+						{
+							sf.put(term, 1);
+						}
+						temp.add(term);
+					}
+					
+					if (!temp2.contains(term))
 					{
 						if (st.df.containsKey(term))
 						{
@@ -123,26 +147,42 @@ public class TFIDF {
 						{
 							st.df.put(term, 1);
 						}
-						
-						if (df.containsKey(term))
-						{
-							Integer tempI = df.get(term);
-							df.remove(term);
-							df.put(term, tempI+1);
-						}
-						else
-						{
-							df.put(term, 1);
-						}
-						temp.add(term);
+						temp2.add(term);
 					}
 					
 				}
 			}
 			subtopic.add(st);
 		}
-		System.out.println(docTotalNum);
 		initCluster(writer);
+	}
+	
+	public void initCluster(PrintWriter writer) throws IOException
+	{
+		int i = 1;
+		while (i < subtopic.size())
+		{
+			double sim = 0;
+			int mergeTo = -1;
+			for (int j = 0; j<i; j++)
+			{
+				double tempsim = similarity(subtopic.elementAt(i), subtopic.elementAt(j));
+				writer.println(subtopic.elementAt(i).summary + " " + subtopic.elementAt(j).summary + " " +tempsim);
+				if (tempsim > InitClusterThreshold && tempsim > sim)
+				{
+					mergeTo = j;
+					sim = tempsim;
+				}
+			}
+			
+			if (mergeTo != -1)
+			{
+				merge(subtopic.elementAt(mergeTo), subtopic.elementAt(i));
+				subtopic.remove(i);
+				i--;
+			}
+			i++;
+		}
 	}
 	
 	public void Threading(BufferedReader reader, PrintWriter writer)
@@ -156,18 +196,21 @@ public class TFIDF {
 				Subtopic st = new Subtopic();
 				st.start = e.start;
 				st.end = e.end;
+				//stTotalNum++;
+				HashSet<String> temp = new HashSet<String>();
 				for (int j = 0; j<e.article.size(); j++)
 				{
-					docTotalNum++;
+					
 					ArticleExtend a = e.article.elementAt(j);
 					st.docNum++;
 					if (st.summary.length() != 0) st.summary += "\n"+a.title; else st.summary += a.title;
 					String[] ss = a.content.split(" ");
-					HashSet<String> temp = new HashSet<String>();
+					HashSet<String> temp2 = new HashSet<String>();
 					for (int k = 0; k<ss.length; k++)
 					{
 						String term = ss[k];
 						if (swf.isStopWord(term)) continue;
+						
 						if (st.tf.containsKey(term))
 						{
 							Integer tempI = st.tf.get(term);
@@ -178,7 +221,23 @@ public class TFIDF {
 						{
 							st.tf.put(term, 1);
 						}
+						
 						if (!temp.contains(term))
+						{						
+							if (sf.containsKey(term))
+							{
+								Integer tempI = sf.get(term);
+								sf.remove(term);
+								sf.put(term, tempI+1);
+							}
+							else
+							{
+								sf.put(term, 1);
+							}
+							temp.add(term);
+						}
+						
+						if (!temp2.contains(term))
 						{
 							if (st.df.containsKey(term))
 							{
@@ -190,18 +249,7 @@ public class TFIDF {
 							{
 								st.df.put(term, 1);
 							}
-							
-							if (df.containsKey(term))
-							{
-								Integer tempI = df.get(term);
-								df.remove(term);
-								df.put(term, tempI+1);
-							}
-							else
-							{
-								df.put(term, 1);
-							}
-							temp.add(term);
+							temp2.add(term);
 						}
 						
 					}
@@ -212,7 +260,7 @@ public class TFIDF {
 				for (int i = 0; i<subtopic.size(); i++)
 				{
 					double tempsim = similarity(subtopic.elementAt(i), st);
-					if (tempsim > InitClusterThreshold && tempsim > sim)
+					if (tempsim > ThreadingThreshold && tempsim > sim)
 					{
 						mergeTo = i;
 						sim = tempsim;
@@ -229,7 +277,7 @@ public class TFIDF {
 				}
 				count++;
 				if (count % 100 == 0) System.out.println(count);
-				//if (count > 1000) break;
+				if (count > TestSample) break;
 			}
 		}
 		catch (Exception e)
@@ -238,43 +286,22 @@ public class TFIDF {
 		}
 	}
 	
-	public void initCluster(PrintWriter writer)
-	{
-		int i = 1;
-		while (i < subtopic.size())
-		{
-			double sim = 0;
-			int mergeTo = -1;
-			for (int j = 0; j<i; j++)
-			{
-				double tempsim = similarity(subtopic.elementAt(i), subtopic.elementAt(j));
-				if (tempsim > InitClusterThreshold && tempsim > sim)
-				{
-					mergeTo = j;
-					sim = tempsim;
-				}
-			}
-			
-			if (mergeTo != -1)
-			{
-				merge(subtopic.elementAt(mergeTo), subtopic.elementAt(i));
-				subtopic.remove(i);
-				i--;
-			}
-			i++;
-		}
-//		for (int j = 0; j<subtopic.size(); j++)
-//		{
-//			writer.println(subtopic.elementAt(j).summary+"\n<subtopic>");
-//			System.out.println(subtopic.elementAt(j).docNum);
-//		}
-//		System.out.println("finished");
-	}
 	
-	public void merge(Subtopic a, Subtopic b)
+	
+	public void merge(Subtopic a, Subtopic b) // merge b into a, but not responsible to delete/remove b. User should delete/remove b in calling function
 	{
 		for (String term : b.tf.keySet())
 		{
+			if (sf.containsKey(term))
+			{
+				if (a.tf.containsKey(term))
+				{
+					Integer temp = sf.get(term);
+					sf.remove(term);
+					sf.put(term, temp-1);
+				}
+			}
+			
 			if (a.tf.containsKey(term))
 			{
 				Integer temp = a.tf.get(term);
@@ -285,6 +312,7 @@ public class TFIDF {
 			{
 				a.tf.put(term, b.tf.get(term));
 			}
+			
 			if (a.df.containsKey(term))
 			{
 				Integer temp = a.df.get(term);
@@ -295,15 +323,16 @@ public class TFIDF {
 			{
 				a.df.put(term, b.df.get(term));
 			}
+			
+			
 		}
-		
 		a.docNum += b.docNum;
 		a.summary += "\n"+b.summary;
 		if (a.start.compareTo(b.start) > 0) a.start = b.start;
 		if (a.end.compareTo(b.end) < 0) a.end = b.end;
 	}
 	
-	public double similarity(Subtopic a, Subtopic b) {
+	public double similarity(Subtopic a, Subtopic b) throws IOException {
 		// TODO Auto-generated method stub
 		double ret = 0;
 		double da = 0.0;
@@ -311,19 +340,26 @@ public class TFIDF {
 		HashMap<String, Double> temp = new HashMap<String, Double>();
 		for (String term : a.tf.keySet())
 		{
-			double tempa = a.tf.get(term) * Math.log((double)docTotalNum/((double)df.get(term)));
+			double tempa = a.tf.get(term) * Math.log((double)subtopic.size()/((double)sf.get(term)));
 			da += tempa*tempa;
 			temp.put(term, tempa);
 		}
 		for (String term : b.tf.keySet())
 		{
-			double tempb = b.tf.get(term) * Math.log((double)docTotalNum/((double)df.get(term)));
+			double tempb = b.tf.get(term) * Math.log((double)subtopic.size()/((double)sf.get(term)));
 			db += tempb*tempb;
 			if (temp.containsKey(term))
 			{
 				ret += temp.get(term).doubleValue() * tempb;
 			}
+//			System.out.println(Math.log((double)subtopic.size()/((double)sf.get(term))+1));
+//			if (sf.get(term) == 0)
+//			{
+//				System.out.println(term);
+//				System.in.read();
+//			}
 		}
+		
 		ret /= Math.sqrt(da*db);
 		return ret;
 	}
